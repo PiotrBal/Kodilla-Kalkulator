33703bfc467dae7a01c252b4e9dd697e168ffcd7
PiotrBal <balcerzakp@gmail.com>
2020-06-15 13:39:29 +0200

    kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,28 +5,19 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args){
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        }else{
-            System.out.println("Error!");
-        }
 
         System.out.println("Test - pierwszy test jednostkowy:");
 
         Calculator calculator = new Calculator();
         int resultAdd = calculator.add(7464, 424);
-        if (resultAdd == resultAdd) {
+        if (resultAdd == 7888) {
             System.out.println("Test OK!");
         }else{
             System.out.println("Error");
         }
 
         int resultSubtrack = calculator.subtract(756,23);
-        if (resultSubtrack == resultSubtrack) {
+        if (resultSubtrack == 733) {
             System.out.println("Test OK!");
         }else{
             System.out.println("Error");
