a4d04e81bea287edeaffb392131013a20d9d9369
PiotrBal <balcerzakp@gmail.com>
2020-06-18 16:30:16 +0200

    Zadanie 6.3 - CollectionTestSuite

--- src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,4 +1,28 @@
 package com.kodilla.testing.collection;
+import java.util.ArrayList;
+import java.util.Random;
 
-public class CollectionTestSuite {
+public class CollectionTestSuite
+{
+    public static void main(String[] args){
+        Random random = new Random();
+        ArrayList<Integer> emptyList = new ArrayList<Integer>();
+        for(int n=0; n<100; n++){
+
+        }
+
+    }
+
+    public static Integer testOddNumbersExterminatorEmptyList() {
+        Integer empty;
+        for(int n=0; n<0; n++){
+            empty = null;
+        }
+        return empty;
+    }
+
+
+    public static Integer testOddNumbersExterminatorNormalList() {
+
+    }
 }
