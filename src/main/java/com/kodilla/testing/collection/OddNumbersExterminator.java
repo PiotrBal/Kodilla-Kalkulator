36eae488f0a0a4707ec089dfb66d002768922529
PiotrBal <balcerzakp@gmail.com>
2020-06-18 15:52:49 +0200

    Zadanie 6.3 - OddNumberExterminator

--- src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -3,11 +3,6 @@ import java.util.ArrayList;
 
 public class OddNumbersExterminator
 {
-    ArrayList<Integer> evenCollection;
-
-    public OddNumbersExterminator(){
-        evenCollection = new ArrayList<Integer>();
-    }
     public void exterminate(ArrayList<Integer> numbers){
         Integer temporaryValue = 0;
         for(int n=0; n>numbers.size(); n++){
