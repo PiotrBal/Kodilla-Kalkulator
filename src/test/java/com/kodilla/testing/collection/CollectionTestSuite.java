b4cb517b0fbc34874a50c5991b5e422606e66995
PiotrBal <balcerzakp@gmail.com>
2020-06-23 14:52:19 +0200

    Zadanie 6.3

--- src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,16 +1,44 @@
 package com.kodilla.testing.collection;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class CollectionTestSuite {
-    public static void main(String[] args) {
-
-        public static Integer testOddNumbersExterminatorEmptyList() {
-            ArrayList<Integer> emptyList = emptyList.EMPTY_empty;
-            Integer emptyList = emptyList.exterminate();
-        }
-        public static Integer testOddNumbersExterminatorNormalList() {
-            ArrayList<Integer> normalList = new ArrayList<Integer>();
-            Integer normalList = normalList.exterminate();
+    @Before
+    public void before() {
+        System.out.println("Collection Test Suite: Start");
+    }
+    @After
+    public void after() {
+        System.out.print("Collection Test Suite: End");
+    }
+    @Test
+    public void testOddNumbersExterminatorEmptyList() {
+        //Given
+        List<Object> emptyList = Collections.emptyList();
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        //When
+        ArrayList<Integer> result = oddNumbersExterminator.exterminate(emptyList);
+        System.out.print(result);
+        //Then
+        Assert.assertEquals(0, result.size());
+    }
+    @Test
+    public void testOddNumbersExterminatorNormalList() {
+        //Given
+        ArrayList<Integer> normalList = new ArrayList<>();
+        for(int n=0; n>50; n++) {
+            OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+            //When
+            ArrayList<Integer> result = oddNumbersExterminator.exterminate(normalList);
+            System.out.print(result);
+            //Then
+            Assert.assertEquals(51, result.size());
         }
     }
 }
